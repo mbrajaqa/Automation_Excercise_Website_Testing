dcfaf69531c25d682e28ee6db4b44033df386120
mbrajaqa <manobharathiraja866@gmail.com>
2026-08-24 19:01:02 +0530

    corrected the whitespaces in TC06

--- tests/TC06_Register_User_with_existing_email.spec.js
+++ tests/TC06_Register_User_with_existing_email.spec.js
@@ -9,8 +9,7 @@ test ('Register user with exixting email', async({page})=>{
 
     const homePage = new HomePage(page);
     const loginPage = new LoginPage(page);
-
-    
+   
     await homePage.openHomePage();
     await homePage.verifyHomePageIsDisplayed();
     await homePage.clickOnSignupOrLoginLink();
@@ -18,15 +17,6 @@ test ('Register user with exixting email', async({page})=>{
     await loginPage.verifyTheNewUserSignUpHeading();
     await loginPage.signup(testUser.data[0].name, testUser.data[0].valid_email);
     await loginPage.verifyEmailAlreadyExistMessage();
-
-    // 1. Launch browser
-    // 2. Navigate to url 'http://automationexercise.com'
-    // 3. Verify that home page is visible successfully
-    // 4. Click on 'Signup / Login' button
-    // 5. Verify 'New User Signup!' is visible
-    // 6. Enter name and already registered email address
-    // 7. Click 'Signup' button
-    // 8. Verify error 'Email Address already exist!' is visible
 
 });
 
